2d97453031558d25acd38e5563e458e40cc6614d
blakeshall <blake@blakeshall.com>
2013-10-20 16:59:11 -0400

    New OOP classes for slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,47 +1,4 @@
-var current_slide = 0;
-
-function change_slide(slide_index){
-  current_slide = slide_index;
-  update_slide();
-}
-
-function update_slide(){
-  $('#slide_title').text(slides[current_slide].title)
-  $('#slide_options').html(''); //empty options
-  // Create each option
-  for (var i=0;i<slides[current_slide].options.length;i++){
-    var href;
-    if (slides[current_slide].options[i].hasOwnProperty('link')){
-      href = slides[current_slide].options[i].link;
-    }
-    else{
-      href = "#"
-    }
-    $('#slide_options').append('<li><a class="btn btn-primary slide_option" href="'+ href +'" data-dest="'+ slides[current_slide].options[i].destination +'">'+ slides[current_slide].options[i].text +'</a></li>')
-  }
-
-  // Click events for each option to continue down the path
-  $('.slide_option').click(function(){
-    if ($(this).data('dest') != null){
-      change_slide($(this).data('dest'));
-    }
-  });
-
-  // Create and link previous button
-  if (current_slide != 0){
-    $('#slide_options').append('<li><a class="btn btn-warning slide_option" href="#">&laquo Back</a></li>')
-    $('#slide_options li a').last().click(function(){
-      change_slide(slides[current_slide].previous);
-    })
-  }
-}
-
-function setup(){
-  update_slide();
-}
-
-
-var slides = [
+var slides_data = [
   {
     title: "I'm a...",
     options: [
@@ -125,29 +82,83 @@ var slides = [
   }
 ];
 
-
-$(document).ready(function(){
-  setup();
-})
-
 // Rewrite for above code. Currently not used.
 
 var Slideshow = function(){
-  this.current_slide = 0;
+  this.current_slide_index = 0;
   this.slides = [];
 }
 
-Slideshow.prototype.change_slide = function(index){
-
+Slideshow.prototype.current_slide = function(){
+  return this.slides[this.current_slide_index];
 }
 
-var Slide = function(title){
+Slideshow.prototype.update_slide = function(){
+  var current_slideshow = this;
+  $('#slide_title').text(current_slideshow.current_slide().title)
+  $('#slide_options').html(''); //empty options
+  // Create each option
+  for (var i=0;i<current_slideshow.current_slide().options.length;i++){
+    var href;
+    if (this.current_slide().options[i].link != null){
+      href = this.current_slide().options[i].link;
+    }
+    else{
+      href = "#"
+    }
+    $('#slide_options').append('<li><a class="btn btn-primary slide_option" href="'+ href +'" data-dest="'+ current_slideshow.current_slide().options[i].destination +'">'+ current_slideshow.current_slide().options[i].text +'</a></li>')
+  }
+
+  // Click events for each option to continue down the path
+  $('.slide_option').click(function(){
+    if ($(this).data('dest') != null){
+      current_slideshow.change_slide($(this).data('dest'));
+    }
+  });
+
+  // Create and link previous button
+  if (this.current_slide_index != 0){
+    $('#slide_options').append('<li><a class="btn btn-warning slide_option" href="#">&laquo Back</a></li>')
+    $('#slide_options li a').last().click(function(){
+      current_slideshow.change_slide(current_slideshow.current_slide().previous);
+    })
+  }
+}
+
+Slideshow.prototype.change_slide = function(index){
+  this.current_slide_index = index;
+  this.update_slide();
+}
+
+var Slide = function(title, previous){
   this.title = title;
+  this.previous = previous;
   this.options = [];
 }
 
 var Option = function(text, destination, link){
+  if(typeof(link)==='undefined') link = null;
   this.text = text;
   this.destination = destination;
   this.link = link;
 }
+
+
+var slideshow = new Slideshow();
+function setup(){
+  for (var i=0; i <= slides_data.length-1; i++){
+    var new_slide = new Slide(slides_data[i].title, slides_data[i].previous);
+    for (var j=0; j < slides_data[i].options.length; j++){
+      var option_data = slides_data[i].options[j];
+      var new_option = new Option(option_data.text, option_data.destination, option_data.link);
+      new_slide.options.push(new_option);
+    }
+    slideshow.slides.push(new_slide);
+  }
+  slideshow.update_slide();
+}
+
+
+$(document).ready(function(){
+  setup();
+})
