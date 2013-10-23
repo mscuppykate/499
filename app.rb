e09adb68dcc2bcfb6edb320339006a2a1404e17c
blakeshall <blake@blakeshall.com>
2013-10-23 14:12:34 -0400

    Moving to a sinatra backend

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'json'
+
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+  end
+end
+
+alert = nil
+
+get '/update_alert' do
+  # Auth check
+  protected!
+
+  alert = params[:text].to_s
+
+end
+
+get '/alert_data.json' do
+  content_type :json
+  {alert_text: alert.to_s}.to_json
+end
